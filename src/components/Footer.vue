96c00c157c4f4cf6ad5fc2241616d7d65de6e641
Hkaar <HkaarDev@gmail.com>
2024-07-27 20:49:57 +0800

    Added Footer Component

---
+++ src/components/Footer.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <footer class="flex items-center justify-center bg-primary text-white px-3 py-4 font-semibold">
+        Copyright &#169; 2024 Hkaar. All rights reserved.
+    </footer>
+</template>
