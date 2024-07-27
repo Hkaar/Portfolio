8863a0fb8372f929d87b00dae9af4b571da9591d
Hkaar <HkaarDev@gmail.com>
2024-07-27 20:51:09 +0800

    Added projects page

---
+++ src/views/Projects.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
+</script>
+
+<template>
+    <Header active="projects"></Header>
+
+    <div class="min-h-screen grid place-items-center">
+        <h1>Homehevgvdegcvdg</h1>
+    </div>
+
+    <Footer></Footer>
+</template>
