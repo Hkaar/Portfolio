bcaa4da57f5ca81cfa03692f239c1b2097d171d5
Hkaar <HkaarDev@gmail.com>
2024-07-27 20:50:43 +0800

    Added home page

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
+</script>
+
+<template>
+    <Header active="home"></Header>
+
+    <section class="min-h-screen flex">
+        <div class="flex-1">
+            Text
+        </div>
+        <div class="flex-1">
+            Image
+        </div>
+    </section>
+    
+    <Footer></Footer>
+</template>
