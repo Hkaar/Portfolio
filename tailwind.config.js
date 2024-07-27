8d3e6092f1268090339440370e4e007ce8b164c7
Hkaar <HkaarDev@gmail.com>
2024-07-27 20:47:55 +0800

    Added color pallete to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "selector",
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#17153B",
+        "secondary": "#2E236C",
+        "tertiary": "#433D8B",
+        "primary-dark": "#C8ACD6",
+        "secondary-dark": "#433D8B",
+        "tertiary-dark": "#2E236C"
+      }
+    },
   },
   plugins: [],
 }
