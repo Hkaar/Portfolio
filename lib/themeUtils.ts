3e4b240730c9c226de96540c3e2545bb011f291e
Hkaar <HkaarDev@gmail.com>
2024-10-15 10:54:39 +0800

    Added new theUtils in lib

---
+++ lib/themeUtils.ts
@@ -0,0 +1,31 @@
+export function loadStoredTheme() {
+    const theme = localStorage.getItem("theme")
+
+    if (theme === "light") {
+        setAppTheme("light")
+    } else {
+        setAppTheme("dark")
+    }
+}
+
+export function getAppTheme() {
+    const theme = localStorage.getItem("theme")
+
+    if (theme) {return theme}
+
+    const root = document.querySelector("html") as HTMLElement
+
+    return root.classList.contains('dark') ? 'dark' : 'light';
+}
+
+export function setAppTheme(theme: string) {
+    const root = document.querySelector("html") as HTMLElement;
+
+    if (theme === "dark") {
+        localStorage.setItem("theme", "dark")
+        root.classList.add("dark")
+    } else {
+        localStorage.setItem("theme", "light")
+        root.classList.remove("dark") 
+    }
+}
