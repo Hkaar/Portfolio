5352ad5280f0bf6f8ed7f6009c4339be3a887133
Hkaar <HkaarDev@gmail.com>
2024-10-15 10:54:07 +0800

    Added new Hero component

---
+++ components/Hero/index.tsx
@@ -0,0 +1,19 @@
+export default function Hero() {
+    return (
+        <section className="min-h-screen flex">
+            <div className="container flex-1 flex justify-between gap-6 items-center">
+                <div className="flex flex-col gap-6">
+                    <div className="flex flex-col gap-3">
+                        <h1 className="font-bold text-8xl">
+                            Hello There!
+                        </h1>
+
+                        <p>
+                            
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
