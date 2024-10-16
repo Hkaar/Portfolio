50475ed53a8c1dfb8e39febe5d85144ebf5f5777
Hkaar <HkaarDev@gmail.com>
2024-10-16 14:15:43 +0800

    Added more colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,7 +24,31 @@ const config: Config = {
         "tertiary-dark": "#8091E5",
         "base-light": "#fafafa",
         "base-dark": "#0b0b0b",
+        "danger": "#dc2626",
+        "danger-dark": "#ef4444",
+        "info": "#3b82f6",
+        "info-dark": "#93c5fd",
+        "warning": "#facc15",
+        "warning-dark": "#fde047",
+        "success": "#16a34a",
+        "success-dark": "#4ade80"
       },
+      animation: {
+        "float-down": "float-down 6s ease infinite"
+      },
+      keyframes: {
+        "float-down": {
+          "0%": {
+            "transform": "translateY(0)"
+          },
+          "50%": {
+            "transform": "translateY(10px)"
+          },
+          "100%": {
+            "transform": "translateY(0)"
+          }
+        }
+      }
     },
   },
   plugins: [],
