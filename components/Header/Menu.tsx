38b95dffa9294d5783ad5e37be2566615732043e
Hkaar <HkaarDev@gmail.com>
2024-10-16 23:22:55 +0800

    Updated header component

--- components/Header/Menu.tsx
+++ components/Header/Menu.tsx
@@ -3,19 +3,26 @@ import React from "react";
 import Icon from "../Icon";
 
 interface MenuProps {
-    href: string;
-    icon?: string;
-    active: boolean;
-    children: React.ReactNode;
+  href: string;
+  icon?: string;
+  active: boolean;
+  children: React.ReactNode;
 }
 
 export default function Menu({ href, icon, active, children }: MenuProps) {
-    return (
-        <Link href={href} passHref className={`${active ? "bg-base-dark dark:bg-base-light text-base-light dark:text-base-dark font-semibold"
-            : "hover:bg-base-dark hover:dark:bg-base-light hover:text-base-light dark:hover:text-base-dark active:opacity-50 active:scale-90 hover:scale-105"}
-            flex items-center gap-2  duration-150 ease-in-out px-3 py-2 rounded-md`}>
-            {icon ? (<Icon>{icon}</Icon>) : null}
-            {children}
-        </Link>
-    );
+  return (
+    <Link
+      href={href}
+      passHref
+      className={`${
+        active
+          ? "bg-base-dark dark:bg-base-light text-base-light dark:text-base-dark font-semibold"
+          : "hover:bg-base-dark hover:dark:bg-base-light hover:text-base-light dark:hover:text-base-dark active:opacity-50 active:scale-90 hover:scale-105"
+      }
+            flex items-center gap-2  duration-150 ease-in-out px-3 py-2 rounded-md`}
+    >
+      {icon ? <Icon>{icon}</Icon> : null}
+      {children}
+    </Link>
+  );
 }
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -10,53 +10,81 @@ import { loadStoredTheme } from "@/lib/themeUtils";
 import { useEffect, useState } from "react";
 
 export default function Header() {
-    const path = usePathname();
-    const [isScrolled, setIsScrolled] = useState(false);
-    const [isCollapsed, setIsCollapsed] = useState(false);
+  const path = usePathname();
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
-    useEffect(() => {
-        loadStoredTheme();
+  useEffect(() => {
+    loadStoredTheme();
 
-        const handleScroll = () => {
-            setIsScrolled(globalThis.scrollY > 35);
-        };
-
-        globalThis.addEventListener("scroll", handleScroll);
-
-        return () => {
-            globalThis.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
-
-    const handleCollapse = () => {
-        setIsCollapsed((prev) => !prev);
+    const handleScroll = () => {
+      setIsScrolled(globalThis.scrollY > 35);
     };
 
-    return (
-        <header className={`py-4 bg-base-light dark:bg-base-dark border-b border-gray-200 dark:border-gray-800 sticky top-0 z-40
-            ${isScrolled ? 'bg-opacity-20 dark:bg-base-light dark:bg-opacity-[7%] backdrop-blur-lg drop-shadow-lg' : ''}`}>
-            <div className="container flex items-center flex-col gap-4 lg:flex-row justify-between">
-                <div className="flex justify-between w-full lg:w-fit lg:justify-start">
-                    <div className="flex lg:items-center justify-center lg:justify-start gap-2">
-                        <Icon size={36}>deployed_code</Icon>
-                        <h5 className="font-semibold text-xl">Shava Jaya</h5>
-                    </div>
+    globalThis.addEventListener("scroll", handleScroll);
 
-                    <Button className="lg:hidden" icon="menu" onClick={handleCollapse} />
-                </div>
+    return () => {
+      globalThis.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-                <div id="navMenu" className={`flex lg:items-center flex-col lg:flex-row gap-3 w-full lg:w-fit ${isCollapsed ? 'block' : 'hidden lg:flex'}`}>
-                    <nav className="flex lg:items-center flex-col lg:flex-row gap-2">
-                        <Menu href="/" active={path === '/'} icon="home">Home</Menu>
-                        <Menu href="/about" active={path === '/about'} icon="info">About</Menu>
-                        <Menu href="/blog" active={path.startsWith('/blog')} icon="newspaper">Blog</Menu>
-                        <Menu href="/projects" active={path.startsWith('/projects')} icon="deployed_code">Projects</Menu>
-                        <Menu href="/contact" active={path.startsWith('/contact')} icon="phone">Contact</Menu>
-                    </nav>
-                    
-                    <ThemeToggle />
-                </div>
-            </div>
-        </header>
-    );
+  const handleCollapse = () => {
+    setIsCollapsed((prev) => !prev);
+  };
+
+  return (
+    <header
+      className={`py-4 bg-base-light dark:bg-base-dark border-b border-gray-200 dark:border-gray-800 sticky top-0 z-40
+            ${
+        isScrolled
+          ? "bg-opacity-20 dark:bg-base-light dark:bg-opacity-[7%] backdrop-blur-lg drop-shadow-lg"
+          : ""
+      }`}
+    >
+      <div className="container flex items-center flex-col gap-4 lg:flex-row justify-between">
+        <div className="flex justify-between w-full lg:w-fit lg:justify-start">
+          <div className="flex lg:items-center justify-center lg:justify-start gap-2">
+            <Icon size={36}>deployed_code</Icon>
+            <h5 className="font-semibold text-xl">Shava Jaya</h5>
+          </div>
+
+          <Button className="lg:hidden" icon="menu" onClick={handleCollapse} />
+        </div>
+
+        <div
+          id="navMenu"
+          className={`flex lg:items-center flex-col lg:flex-row gap-3 w-full lg:w-fit ${
+            isCollapsed ? "block" : "hidden lg:flex"
+          }`}
+        >
+          <nav className="flex lg:items-center flex-col lg:flex-row gap-2">
+            <Menu href="/" active={path === "/"} icon="home">Home</Menu>
+            <Menu
+              href="/projects"
+              active={path.startsWith("/projects")}
+              icon="deployed_code"
+            >
+              Projects
+            </Menu>
+            <Menu
+              href="/blog"
+              active={path.startsWith("/blog")}
+              icon="newspaper"
+            >
+              Blog
+            </Menu>
+            <Menu
+              href="/contact"
+              active={path.startsWith("/contact")}
+              icon="phone"
+            >
+              Contact
+            </Menu>
+          </nav>
+
+          <ThemeToggle />
+        </div>
+      </div>
+    </header>
+  );
 }
