9a34aedf6fbdef7e8d007094d8493f77bfa84632
Hkaar <HkaarDev@gmail.com>
2024-10-16 23:21:53 +0800

    Updated input field

--- components/InputField/index.tsx
+++ components/InputField/index.tsx
@@ -1,34 +1,34 @@
 interface InputFieldProps extends React.HTMLAttributes<HTMLInputElement> {
-    title?: string;
-    labelClassName?: string;
-    name?: string;
+  title?: string;
+  labelClassName?: string;
+  name?: string;
 }
 
 export default function InputField(
-    { className, labelClassName, title, name, ...props }: InputFieldProps,
+  { className, labelClassName, title, name, ...props }: InputFieldProps,
 ) {
-    return (
-        <div className="space-y-1">
-            {title
-                ? (
-                    <label
-                        className={`block ${labelClassName} font-medium`}
-                        htmlFor={props.id}
-                    >
-                        {title}
-                    </label>
-                )
-                : ""}
+  return (
+    <div className="space-y-1">
+      {title
+        ? (
+          <label
+            className={`block ${labelClassName} font-medium`}
+            htmlFor={props.id}
+          >
+            {title}
+          </label>
+        )
+        : ""}
 
-            <div className="relative">
-                <input
-                    type={props.datatype}
-                    className={`${className} block rounded-md outline-none border px-3 py-2 border-gray-200 dark:border-gray-800 
-                    shadow-sm focus:ring focus:ring-accent dark:focus:ring-primary-dark text-base-dark dark:bg-base-light`}
-                    name={name}
-                    id={props.id}
-                />
-            </div>
-        </div>
-    );
+      <div className="relative">
+        <input
+          type={props.datatype}
+          className={`${className} block rounded-md outline-none border px-3 py-2 border-gray-200 dark:border-gray-800 
+                    shadow-sm focus:ring focus:ring-info dark:focus:ring-info-dark text-base-dark dark:bg-base-dark dark:text-base-light`}
+          name={name}
+          id={props.id}
+        />
+      </div>
+    </div>
+  );
 }
