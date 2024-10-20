d656ba510c519647851ba5bd8c4c012cef3dce4c
Hkaar <HkaarDev@gmail.com>
2024-10-20 20:10:03 +0800

    Updated section content component

--- components/ContentSection/SectionContent.tsx
+++ components/ContentSection/SectionContent.tsx
@@ -1,20 +1,29 @@
+import { twMerge } from "tailwind-merge";
+
 interface SectionContentProps extends React.HTMLAttributes<HTMLElement> {
   title: string;
   centered?: boolean;
+  descriptionClassName?: string;
 }
 
 export default function SectionContent(
-  { title, centered, ...props }: SectionContentProps,
+  { title, centered, descriptionClassName, ...props }: SectionContentProps,
 ) {
   return (
     <div
-      className={`${
-        centered ? "text-center items-center" : "text-center lg:text-start"
-      } flex-1 flex flex-col gap-5 ${props.className}`}
+      className={twMerge(
+        "flex-1 flex flex-col gap-5",
+        centered ? "text-center items-center" : "text-center lg:text-start",
+        props.className,
+      )}
     >
-      <h3 className="font-bold text-2xl md:text-4xl xl:text-5xl">{title}</h3>
+      <h3 className="font-bold text-2xl md:text-4xl xl:text-5xl w-3/4">
+        {title}
+      </h3>
 
-      <p className="text-lg md:text-xl text-gray-400 tracking-wide leading-relaxed">
+      <p
+        className={`text-lg md:text-xl text-gray-400 tracking-wide leading-relaxed ${descriptionClassName}`}
+      >
         {props.children}
       </p>
     </div>
