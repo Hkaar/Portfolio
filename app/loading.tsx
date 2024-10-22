cfbb2cad07bde61623277b1a826f64b9d507bfea
Hkaar <HkaarDev@gmail.com>
2024-10-22 15:53:55 +0800

    Fixed layout in root loading page

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,10 +4,10 @@ export default function Loading() {
   return (
     <div className="min-h-screen flex">
       <div className="container grid place-items-center flex-1">
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col items-center gap-3">
           <LoadSpinner />
 
-          <h4 className="text-2xl font-bold">Loading ...</h4>
+          <h4 className="text-2xl font-bold">Loading</h4>
         </div>
       </div>
     </div>
