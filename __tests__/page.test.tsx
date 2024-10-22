42114285553aeea023fde922fbbf64774b508f9d
Hkaar <HkaarDev@gmail.com>
2024-10-22 22:24:26 +0800

    Fixed async page not passing test due to returning [Object]

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,8 +1,9 @@
-import { expect, test } from 'vitest'
-import { render, screen } from '@testing-library/react'
-import Home from '../app/(main)/page';
- 
-test('Page', () => {
-  render(<Home />)
-  expect(screen.getByRole('heading', { level: 3, name: 'About myself' })).toBeDefined()
-})
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Home from "../app/(main)/page";
+
+test("Page", async () => {
+  render(await (async () => await Home())());
+  expect(screen.getByRole("heading", { level: 3, name: "About myself" }))
+    .toBeDefined();
+});
