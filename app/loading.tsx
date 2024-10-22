298be3a337690acca0940f6fe2a0c314cc505a46
Hkaar <HkaarDev@gmail.com>
2024-10-22 13:57:05 +0800

    Added root loading page

---
+++ app/loading.tsx
@@ -0,0 +1,15 @@
+import LoadSpinner from "@/components/Loader/LoadSpinner";
+
+export default function Loading() {
+  return (
+    <div className="min-h-screen flex">
+      <div className="container grid place-items-center flex-1">
+        <div className="flex flex-col gap-3">
+          <LoadSpinner />
+
+          <h4 className="text-2xl font-bold">Loading ...</h4>
+        </div>
+      </div>
+    </div>
+  )
+}
