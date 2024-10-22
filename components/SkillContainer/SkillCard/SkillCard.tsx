16e360ab2595ada4de2ae70feada755da0f940ab
Hkaar <HkaarDev@gmail.com>
2024-10-22 10:09:44 +0800

    Updated props in skill card

--- components/SkillContainer/SkillCard/SkillCard.tsx
+++ components/SkillContainer/SkillCard/SkillCard.tsx
@@ -2,15 +2,14 @@ import { Icon } from "@iconify/react";
 
 interface SkillCardProps extends React.HTMLAttributes<HTMLElement> {
   icon: string;
-  title: string;
 }
 
-export default function SkillCard({ icon, title, ...props }: SkillCardProps) {
+export default function SkillCard({ icon, ...props }: SkillCardProps) {
   return (
-    <div className={`${props.className} flex flex-col items-center justify-center gap-3 p-6 rounded-md border border-gray-200 dark:border-gray-800 bg-white dark:bg-base-dark text-center`}>
+    <div {...props} className={`${props.className} flex flex-col items-center justify-center gap-3 p-6 rounded-md border border-gray-200 dark:border-gray-800 bg-white dark:bg-base-dark text-center`}>
       <Icon icon={icon} fontSize={48} fontWeight={300} className="dark:fill-base-dark"></Icon>
 
-      <span className="font-semibold">{title}</span>
+      {props.children}
     </div>
   );
 }
