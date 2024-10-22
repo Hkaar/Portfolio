478b106f9c91ac8a0ed6c405fe4a0ad56a6c6c46
Hkaar <HkaarDev@gmail.com>
2024-10-22 22:05:10 +0800

    Fixed sanity error in tests

--- lib/client.ts
+++ lib/client.ts
@@ -1,9 +1,9 @@
 import { createClient } from "@sanity/client";
 
 const client = createClient({
-    projectId: process.env.SANITY_PROJECT_ID,
+    projectId: process.env.SANITY_PROJECT_ID ? process.env.SANITY_PROJECT_ID : 'aefnjm75',
     apiVersion: process.env.SANITY_API_VERSION ? process.env.SANITY_API_VERSION : "v2022-03-07",
-    dataset: process.env.SANITY_DATASET,
+    dataset: process.env.SANITY_DATASET ? process.env.SANITY_PROJECT_ID : 'production',
     token: process.env.SANITY_API_TOKEN,
     useCdn: false,
 });
