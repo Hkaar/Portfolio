f178a98893116394b1ae124cd855339a19fc3f37
Hkaar <HkaarDev@gmail.com>
2024-10-22 22:27:54 +0800

    Fixed error in sanity client

--- lib/client.ts
+++ lib/client.ts
@@ -3,7 +3,7 @@ import { createClient } from "@sanity/client";
 const client = createClient({
     projectId: process.env.SANITY_PROJECT_ID ? process.env.SANITY_PROJECT_ID : 'aefnjm75',
     apiVersion: process.env.SANITY_API_VERSION ? process.env.SANITY_API_VERSION : "v2022-03-07",
-    dataset: process.env.SANITY_DATASET ? process.env.SANITY_PROJECT_ID : 'production',
+    dataset: process.env.SANITY_DATASET ? process.env.SANITY_DATASET : 'production',
     token: process.env.SANITY_API_TOKEN,
     useCdn: false,
 });
