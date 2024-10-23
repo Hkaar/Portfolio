0e7bb771522686f02aa36ec72f95e229a1f9e30e
Hkaar <HkaarDev@gmail.com>
2024-10-23 14:16:40 +0800

    Added card section skeleton

---
+++ components/Skeletons/CardSectionSkeleton/CardSectionSkeleton.tsx
@@ -0,0 +1,30 @@
+import LinkButton from "@/components/LinkButton";
+import CardLoader from "@/components/Loader/CardLoader";
+import { twMerge } from "tailwind-merge";
+
+export default function CardSectionSkeleton({ ...props }: React.HTMLAttributes<HTMLElement>) {
+  return (
+    <div
+      {...props}
+      className={twMerge("flex flex-col gap-6 justify-center items-center", props.className)}
+    >
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <CardLoader />
+        <CardLoader />
+        <CardLoader />
+        <CardLoader className="lg:hidden" />
+        <CardLoader className="lg:hidden" />
+        <CardLoader className="lg:hidden" />
+      </div>
+
+      <LinkButton
+        href="/blog"
+        type="tertiary"
+        icon="material-symbols-light:arrow-right-alt"
+        rightIcon
+      >
+        See more blogs
+      </LinkButton>
+    </div>
+  )
+}
