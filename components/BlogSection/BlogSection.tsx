093d0ec1f78ef5c828d9ebfb74dd1d5f093a05ab
Hkaar <HkaarDev@gmail.com>
2024-10-23 11:15:48 +0800

    Connected blog section to cms

--- components/BlogSection/BlogSection.tsx
+++ components/BlogSection/BlogSection.tsx
@@ -3,42 +3,70 @@ import BlogCard from "../Card/BlogCard";
 import LinkButton from "../LinkButton";
 import CardLoader from "../Loader/CardLoader";
 import SlideUp from "../Transitions/SlideUp";
+import client from "@/lib/client";
 
-export default function BlogSection(
+type Slug = {
+  current: string;
+  _type: string;
+};
+
+type Post = {
+  title: string;
+  slug: Slug;
+  publishedAt: string;
+  categories: Array<string>;
+  icons: Array<string>;
+  author: string;
+  image: string;
+  intro: string;
+  body: Object;
+};
+
+const formatDate = (date: string) => {
+  const d = new Date(date);
+  return d.toLocaleDateString("en-us", {
+    day: "numeric",
+    year: "numeric",
+    month: "long",
+  });
+};
+
+export default async function BlogSection(
   { ...props }: React.HTMLAttributes<HTMLElement>,
 ) {
+  const posts: Array<Post> = await client.fetch(`*[_type == "post"][0..5] {
+    title,
+    slug,
+    body,
+    "categories": categories[0..1]->title,
+    "icons": categories[0..1]->icon->icon,
+    "author": author->name,
+    intro,
+    publishedAt  
+  }`);
+
   return (
     <div
+      {...props}
       className={`${props.className} flex flex-col gap-6 justify-center items-center`}
     >
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <SlideUp>
-          <Suspense fallback={<CardLoader />}>
-            <BlogCard
-              src="https://placehold.co/600x480"
-              title="Storage link not working?"
-              slug="storage-link-not-working"
-              date="18 October 2024"
-              author="Shava Jaya"
-              desc="Ever wondered why storage links in a shared hosting environment for laravel don't work correctly?"
-              topic={{ name: "Laravel", icon: "devicon:laravel" }}
-            />
-          </Suspense>
-        </SlideUp>
-
-        <SlideUp delay={1.4}>
-          <Suspense fallback={<CardLoader />}>
-            <BlogCard
-              src="https://placehold.co/600x480"
-              title="Storage link not working?"
-              slug="storage-link-not-working"
-              date="18 October 2024"
-              author="Shava Jaya"
-              desc="Ever wondered why storage links in a shared hosting environment for laravel don't work correctly?"
-              topic={{ name: "Laravel", icon: "devicon:laravel" }}
-            />
-          </Suspense>
-        </SlideUp>
+        {posts.map((post, i) => (
+          <SlideUp delay={1 + (0.2 * i)} key={post.slug.current}>
+            <Suspense fallback={<CardLoader />}>
+              <BlogCard
+                src="https://placehold.co/600x480"
+                title={post.title}
+                slug={post.slug.current ? post.slug.current : ""}
+                date={formatDate(post.publishedAt)}
+                author={post.author}
+                desc={post.intro ? post.intro : ''}
+                topics={post.categories}
+                topicIcons={post.icons}
+              />
+            </Suspense>
+          </SlideUp>
+        ))}
 
         <SlideUp delay={1.8}>
           <Suspense fallback={<CardLoader />}>
@@ -49,7 +77,8 @@ export default function BlogSection(
               date="18 October 2024"
               author="Shava Jaya"
               desc="Ever wondered why storage links in a shared hosting environment for laravel don't work correctly?"
-              topic={{ name: "Laravel", icon: "devicon:laravel" }}
+              topics={["Laravel"]}
+              topicIcons={["devicon:laravel"]}
             />
           </Suspense>
         </SlideUp>
@@ -64,7 +93,8 @@ export default function BlogSection(
               date="18 October 2024"
               author="Shava Jaya"
               desc="Ever wondered why storage links in a shared hosting environment for laravel don't work correctly?"
-              topic={{ name: "Laravel", icon: "devicon:laravel" }}
+              topics={["Laravel"]}
+              topicIcons={["devicon:laravel"]}
             />
           </Suspense>
         </SlideUp>
@@ -79,7 +109,8 @@ export default function BlogSection(
               date="18 October 2024"
               author="Shava Jaya"
               desc="Ever wondered why storage links in a shared hosting environment for laravel don't work correctly?"
-              topic={{ name: "Laravel", icon: "devicon:laravel" }}
+              topics={["Laravel"]}
+              topicIcons={["devicon:laravel"]}
             />
           </Suspense>
         </SlideUp>
@@ -94,7 +125,8 @@ export default function BlogSection(
               date="18 October 2024"
               author="Shava Jaya"
               desc="Ever wondered why storage links in a shared hosting environment for laravel don't work correctly?"
-              topic={{ name: "Laravel", icon: "devicon:laravel" }}
+              topics={["Laravel"]}
+              topicIcons={["devicon:laravel"]}
             />
           </Suspense>
         </SlideUp>
