c323f383f168ec94a0462a471e1f960bfccda0e8
Hkaar <HkaarDev@gmail.com>
2024-10-23 11:14:44 +0800

    Restyled experience section to fit one card

--- components/ExperienceSection/ExperienceSection.tsx
+++ components/ExperienceSection/ExperienceSection.tsx
@@ -7,37 +7,20 @@ export default function ExperienceSection(
 ) {
   return (
     <div
-      className={`${props.className} grid grid-cols-1 md:grid-cols-2 gap-4 w-full`}
+      className={`${props.className} flex justify-center gap-4 w-full`}
     >
-      <SlideRight>
-        <JobCard
-          className="w-full"
-          title="PT. Taksu Teknologi Indonesia"
-          job="Internship"
-          date="October 2024 - Present"
-          desc="Still ongoing internship process"
-          link="https://www.taksu.tech"
-          topics={[
-            { name: "Next.js", icon: "nextjs" },
-            { name: "Tailwind CSS", icon: "tailwindcss" },
-          ]}
-        />
-      </SlideRight>
-
-      <SlideLeft>
-        <JobCard
-          className="flex-1"
-          title="PT. Taksu Teknologi Indonesia"
-          job="Internship"
-          date="October 2024 - Present"
-          desc="Still ongoing internship process"
-          link="https://www.taksu.tech"
-          topics={[
-            { name: "Next.js", icon: "nextjs" },
-            { name: "Tailwind CSS", icon: "tailwindcss" },
-          ]}
-        />
-      </SlideLeft>
+      <JobCard
+        className="w-full lg:w-1/2"
+        title="PT. Taksu Teknologi Indonesia"
+        job="Internship"
+        date="October 2024 - Present"
+        desc="Still ongoing internship process"
+        link="https://www.taksu.tech"
+        topics={[
+          { name: "Next.js", icon: "nextjs" },
+          { name: "Tailwind CSS", icon: "tailwindcss" },
+        ]}
+      />
     </div>
   );
 }
