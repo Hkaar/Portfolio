23710ae3a6624a3837fbcbde641bd9e6c3e83ed9
Hkaar <HkaarDev@gmail.com>
2024-10-25 00:31:56 +0800

    Added not found pages for dynamic routes

---
+++ app/(dynamic)/blog/[slug]/not-found.tsx
@@ -0,0 +1,25 @@
+import LinkButton from "@/components/LinkButton";
+
+export default function NotFoundPage() {
+  return (
+    <>
+      <div className="min-h-screen grid place-items-center">
+        <div className="flex flex-col items-center text-center gap-5">
+          <div className="flex flex-col items-center gap-2">
+            <h1 className="text-8xl font-bold">
+              404
+            </h1>
+
+            <p className="text-gray-400 text-3xl leading-relaxed tracking-wide">
+              Sorry, looks like that blog doesn’t exist!
+            </p>
+          </div>
+
+          <div className="flex justify-center items-center gap-4">
+            <LinkButton href="/blog" type="primary" icon="mdi-light:arrow-left">Go back to index</LinkButton>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
