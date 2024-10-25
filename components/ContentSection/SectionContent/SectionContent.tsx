2a3c09a950cc4fb217c64a0cd47124f3d4abf1e8
Hkaar <HkaarDev@gmail.com>
2024-10-25 23:57:41 +0800

    Updated content section components

--- components/ContentSection/SectionContent/SectionContent.tsx
+++ components/ContentSection/SectionContent/SectionContent.tsx
@@ -13,7 +13,9 @@ export default function SectionContent(
     <div
       className={twMerge(
         "flex-1 flex flex-col gap-2 md:gap-3",
-        centered ? "text-center items-center" : "text-center lg:text-start items-center lg:items-start",
+        centered
+          ? "text-center items-center"
+          : "text-center lg:text-start items-center lg:items-start",
         props.className,
       )}
     >
@@ -22,7 +24,10 @@ export default function SectionContent(
       </h3>
 
       <div
-        className={twMerge("md:text-lg lg:text-xl text-gray-400 tracking-wide leading-relaxed", descriptionClassName)}
+        className={twMerge(
+          "md:text-lg lg:text-xl text-gray-400 tracking-wide leading-relaxed",
+          descriptionClassName,
+        )}
       >
         {props.children}
       </div>
