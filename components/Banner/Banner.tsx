39966393363e4006499d33327b94e03f3c93074f
Hkaar <HkaarDev@gmail.com>
2024-10-25 19:40:18 +0800

    Updated banner

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -12,25 +12,11 @@ export default function Banner({ src, ...props }: BannerProps) {
     <div
       {...props}
       className={twMerge(
-        "h-[80vh] relative",
+        "h-[80vh] relative grid grid-cols-1 lg:grid-cols-3 bg-base-dark text-base-light",
         props.className,
       )}
     >
-      <Image
-        src={src}
-        alt="Image failed to load..."
-        height={1080}
-        width={1920}
-        className="object-cover aspect-video z-0 block h-full"
-      />
-
-      <div className="absolute z-20 bg-base-dark bg-opacity-5 backdrop-blur top-0 right-0 w-[69%] h-full">
-      </div>
-
-      <div className="bg-base-dark absolute h-full z-10 top-0 right-0 w-[5%]">
-      </div>
-
-      <div className="bg-base-dark absolute h-full z-10 top-0 left-0 w-1/3 text-base-light justify-end px-6 py-12 flex flex-col gap-8">
+      <div className="justify-end px-6 py-12 flex flex-col gap-8 order-2 lg:order-1">
         <div className="flex flex-col gap-3">
           <h1 className="text-6xl font-bold">Seven Books</h1>
 
@@ -72,6 +58,19 @@ export default function Banner({ src, ...props }: BannerProps) {
           <span className="font-semibold">
             Scroll down for more info
           </span>
+        </div>
+      </div>
+
+      <div className="relative lg:h-full col-span-2 order-1 lg:order-2">
+        <Image
+          src={src}
+          alt="Image failed to load..."
+          height={1080}
+          width={1920}
+          className="object-cover aspect-video block lg:h-full"
+        />
+
+        <div className="absolute z-10 bg-base-dark bg-opacity-5 backdrop-blur top-0 lg:left-0 w-full h-full lg:w-full lg:h-full">
         </div>
       </div>
     </div>
