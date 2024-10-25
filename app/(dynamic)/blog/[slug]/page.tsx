126280724299aa8a54558c7e0e31f00763af8274
Hkaar <HkaarDev@gmail.com>
2024-10-25 23:55:34 +0800

    Updated look of dynamic pages

--- app/(dynamic)/blog/[slug]/page.tsx
+++ app/(dynamic)/blog/[slug]/page.tsx
@@ -6,8 +6,9 @@ import Profile from "@/components/Profile";
 import Badge from "@/components/Badge";
 import { Icon } from "@iconify/react";
 import client from "@/lib/client";
-import { PortableTextBlock } from "@portabletext/react";
+import { PortableText, PortableTextBlock } from "@portabletext/react";
 import { notFound } from "next/navigation";
+import { formatDate } from "@/lib/commonUtils";
 
 type Slug = {
   current: string;
@@ -27,25 +28,29 @@ type Blog = {
 };
 
 interface BlogPageProps {
-  params: Promise<{slug: string}>
+  params: Promise<{ slug: string }>;
 }
 
 async function getBlog(slug: string) {
-  const response: Blog = await client.fetch(`*[_type == "post" && slug.current == "${slug}"][0] {
+  const response: Blog = await client.fetch(
+    `*[_type == "post" && slug.current == "${slug}"][0] {
     title,
     slug,
     body,
     "image": image.asset->url,
-    "categories": categories[0..1]->title,
-    "icons": categories[0..1]->icon->icon,
+    "categories": categories[]->title,
+    "icons": categories[]->icon->icon,
     "author": author->name,
     intro,
     publishedAt  
-  }`);
+  }`,
+  );
 
   if (response === null) return undefined;
-  return response
+  return response;
 }
+
+export const fetchCache = 'force-no-store';
 
 export default async function BlogPage(props: BlogPageProps) {
   const params = await props.params;
@@ -60,30 +65,24 @@ export default async function BlogPage(props: BlogPageProps) {
       <ArticleContainer>
         <ArticleHeader>
           <Image
-            src="https://placehold.co/600x480"
-            alt="No Image available"
+            src={blog.image || "https://placehold.co/600x480"}
+            alt="Image failed to load ..."
             width={1920}
             height={1080}
             className="w-full h-96 object-cover"
           />
 
           <div className="flex items-center gap-2">
-            <Badge icon="devicon:laravel">
-              Laravel
-            </Badge>
-
-            <Badge icon="devicon:bootstrap">
-              Bootstrap
-            </Badge>
-
-            <Badge icon="devicon:mysql">
-              MySQL
-            </Badge>
+            {blog.categories.map((category, i) => (
+              <Badge key={category} icon={blog.icons[i] || "material-symbols-light:tag"}>
+                <span className="text-sm text-gray-500">{category}</span>
+              </Badge>
+            ))}
           </div>
 
           <div className="flex flex-col gap-4">
             <h1 className="text-5xl font-bold tracking-tighter">
-              Common Storage Problems in Laravel
+              {blog.title}
             </h1>
 
             <div className="flex items-center gap-2 tracking-wide text-gray-400">
@@ -93,7 +92,7 @@ export default async function BlogPage(props: BlogPageProps) {
                 fontWeight={400}
               />
 
-              Febuary 24, 2024
+              {formatDate(blog.publishedAt)}
             </div>
           </div>
 
@@ -104,67 +103,11 @@ export default async function BlogPage(props: BlogPageProps) {
               src="https://placehold.co/600x480"
             />
           </div>
+
+          <hr />
         </ArticleHeader>
 
-        <p className="tracking-wide leading-relaxed">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime, sit?
-          Sapiente magni debitis tempore amet rerum porro, consequuntur, nulla
-          beatae quisquam delectus voluptatum deserunt iure, fugit quos modi
-          quis ut?
-
-          {params.slug}
-        </p>
-
-        <h2 className="text-4xl font-bold tracking-tight">
-          Heading 1
-        </h2>
-
-        <h3 className="text-3xl font-bold tracking-tight">
-          Heading 2
-        </h3>
-
-        <h4 className="text-2xl font-bold tracking-tight">
-          Heading 3
-        </h4>
-
-        <ArticleBlock>
-          <h2 className="text-4xl font-bold tracking-tight">
-            Heading 1
-          </h2>
-
-          <p className="tracking-wide leading-relaxed">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-            sit? Sapiente magni debitis tempore amet rerum porro, consequuntur,
-            nulla beatae quisquam delectus voluptatum deserunt iure, fugit quos
-            modi quis ut?
-          </p>
-        </ArticleBlock>
-
-        <ArticleBlock>
-          <h3 className="text-3xl font-bold tracking-tight">
-            Heading 2
-          </h3>
-
-          <p className="tracking-wide leading-relaxed">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-            sit? Sapiente magni debitis tempore amet rerum porro, consequuntur,
-            nulla beatae quisquam delectus voluptatum deserunt iure, fugit quos
-            modi quis ut?
-          </p>
-        </ArticleBlock>
-
-        <ArticleBlock>
-          <h4 className="text-2xl font-bold tracking-tight">
-            Heading 3
-          </h4>
-
-          <p className="tracking-wide leading-relaxed">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime,
-            sit? Sapiente magni debitis tempore amet rerum porro, consequuntur,
-            nulla beatae quisquam delectus voluptatum deserunt iure, fugit quos
-            modi quis ut?
-          </p>
-        </ArticleBlock>
+        <PortableText value={blog.body} />
       </ArticleContainer>
     </>
   );
