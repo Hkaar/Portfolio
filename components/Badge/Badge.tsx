1ee2b6f553e79188eccc74a9e4d3d13da3654a48
Hkaar <HkaarDev@gmail.com>
2024-10-25 23:58:11 +0800

    Adjusted padding and formatting of badge component

--- components/Badge/Badge.tsx
+++ components/Badge/Badge.tsx
@@ -7,10 +7,25 @@ interface BadgeProps extends React.HTMLAttributes<HTMLElement> {
 
 export default function Badge({ icon, className, ...props }: BadgeProps) {
   return (
-    <span {...props} className={twMerge("px-3 py-2 border rounded-md shadow flex items-center gap-2 justify-center border-gray-200 dark:border-gray-800", className)}>
+    <span
+      {...props}
+      className={twMerge(
+        "px-2 py-1 border rounded-md shadow flex items-center gap-2 justify-center border-gray-200 dark:border-gray-800",
+        className,
+      )}
+    >
       {icon
-        ? <Icon icon={icon} fontWeight={300} fontSize={24} className="dark:fill-base-dark"></Icon>
+        ? (
+          <Icon
+            icon={icon}
+            fontWeight={300}
+            fontSize={24}
+            className="dark:fill-base-dark"
+          >
+          </Icon>
+        )
         : null}
+
       {props.children}
     </span>
   );
