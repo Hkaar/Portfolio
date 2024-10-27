53b0d8e73394bbae848b06aab77a31abd03d2cb6
Hkaar <HkaarDev@gmail.com>
2024-10-27 14:15:57 +0800

    Added class prop to transition slide

--- components/Transitions/SlideUp/SlideUp.tsx
+++ components/Transitions/SlideUp/SlideUp.tsx
@@ -1,4 +1,5 @@
 import * as motion from "framer-motion/client";
+import { twMerge } from "tailwind-merge";
 
 interface SlideUpProps extends React.HTMLAttributes<HTMLElement> {
   delay?: number
@@ -6,10 +7,10 @@ interface SlideUpProps extends React.HTMLAttributes<HTMLElement> {
 }
 
 export default function SlideUp(
-  { delay, repeat, children }: SlideUpProps,
+  { delay, repeat, children, className }: SlideUpProps,
 ) {
   return (
-    <motion.div className="w-full"
+    <motion.div className={twMerge("w-full", className)}
       initial={{ y: 50, opacity: 0, z: 30 }}
       whileInView={{ y: 0, opacity: 1, z: 0, transition: { duration: delay ? delay : 1.2 } }}
       viewport={{ once: repeat ? false : true }}
