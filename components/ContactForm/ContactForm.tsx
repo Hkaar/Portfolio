d229cca96be432d3d067a9629ffc91a6144316a5
Hkaar <HkaarDev@gmail.com>
2024-10-27 14:20:24 +0800

    Made contact form work

--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -1,27 +1,111 @@
+"use client";
+
 import { twMerge } from "tailwind-merge";
 import Button from "../Button";
 import InputField from "../InputField";
 import TextArea from "../TextArea";
 
-export default function ContactForm({ ...props }: React.HTMLAttributes<HTMLFormElement>) {
+import { FormEvent, useState } from "react";
+
+export default function ContactForm(
+  { ...props }: React.HTMLAttributes<HTMLFormElement>,
+) {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setResult("Sending....");
+
+    const formData = new FormData(event.currentTarget);
+
+    formData.append("access_key", "45ee2ba9-7c2c-4dd1-a55b-3968ca96b088");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
-    <form className={twMerge("px-8 py-6 border border-gray-200 dark:border-gray-800 rounded-md shadow-md", props.className)}>
+    <form
+      onSubmit={onSubmit}
+      className={twMerge(
+        "px-8 py-6 border border-neutral-200 dark:border-neutral-800 rounded-md shadow-md",
+        props.className,
+      )}
+    >
       <div className="flex flex-col gap-6">
         <div className="space-y-3">
-          <div className="flex items-center gap-3">
-            <InputField title="First name" datatype="text" placeholder="Your first name ..." required />
-            <InputField title="Last name" datatype="text" placeholder="Your last name ..." required />
-          </div>
+          {/* <div className="flex items-center gap-3">
+            <InputField
+              title="First name"
+              datatype="text"
+              name="first name"
+              className="flex-1"
+              placeholder="Your first name ..."
+              required
+            />
+            <InputField
+              title="Last name"
+              datatype="text"
+              name="last name"
+              className="flex-1"
+              placeholder="Your last name ..."
+              required
+            />
+          </div> */}
 
-          <InputField title="Email" datatype="email" className="w-full" placeholder="Your email address ..." required />
+          <InputField
+            title="Name"
+            name="name"
+            datatype="text"
+            placeholder="Enter your name ..."
+            required
+          />
 
-          <InputField title="Subject" placeholder="What do you want to discuss?" required />
+          <InputField
+            title="Email"
+            name="email"
+            datatype="email"
+            className="w-full"
+            placeholder="Enter your email address ..."
+            required
+          />
 
-          <TextArea title="Message" className="w-full" placeholder="Enter your message here ..." />
+          <InputField
+            title="Subject"
+            name="subject"
+            datatype="text"
+            placeholder="What do you want to discuss?"
+            required
+          />
+
+          <TextArea
+            title="Message"
+            name="message"
+            className="w-full"
+            placeholder="Enter your message here ..."
+          />
+
+          <span>{result}</span>
         </div>
 
         <div className="flex items-center gap-2">
-          <Button icon="material-symbols-light:send-outline" type="primary" rightIcon>
+          <Button
+            icon="material-symbols-light:send-outline"
+            type="primary"
+            rightIcon
+            actionType="submit"
+          >
             Send message
           </Button>
         </div>
