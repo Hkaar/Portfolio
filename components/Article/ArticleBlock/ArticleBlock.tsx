b7432ff5002cfb6d42bd46e38e674ebd577f94fb
Hkaar <HkaarDev@gmail.com>
2024-10-27 14:15:31 +0800

    Removed article block component

--- components/Article/ArticleBlock/ArticleBlock.tsx
+++
@@ -1,9 +0,0 @@
-import { twMerge } from "tailwind-merge"
-
-export default function ArticleBlock({...props}: React.HTMLAttributes<HTMLElement>) {
-  return (
-    <div {...props} className={twMerge("flex flex-col gap-3 tracking-wide leading-relaxed", props.className)}>
-      {props.children}
-    </div>
-  )
-}
