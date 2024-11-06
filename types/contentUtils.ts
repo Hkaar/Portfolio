d0f1469be8de01d2b8e9869013b91666a245ab66
Hkaar <HkaarDev@gmail.com>
2024-11-06 08:39:02 +0800

    Updated types

---
+++ types/contentUtils.ts
@@ -0,0 +1,14 @@
+export type Language = {
+    name: string;
+    icon?: string;
+    color: string;
+    percentage?: number;
+}
+
+export type Platform = {
+    name: string;
+    icon: string;
+    status?: string;
+    statusIcon?: string;
+    statusColor?: string;
+}
