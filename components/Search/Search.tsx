58add1b3fd2616e2df7c79e7afc086e097459753
Hkaar <HkaarDev@gmail.com>
2024-11-11 16:34:33 +0800

    Added new search component

---
+++ components/Search/Search.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useState } from "react";
+import Button from "../Button";
+import FilterModal from "../Modal/FilterModal";
+import SearchBox from "../SearchBox";
+import { Category } from "@/types/contentUtils";
+
+interface SearchProps {
+  placeholder?: string;
+  categories: Category[]
+}
+
+export default function Search({ placeholder, categories }: SearchProps) {
+  const [open, setOpen] = useState(false);
+
+  const openModal = () => setOpen(true);
+  const closeModal = () => setOpen(false);
+
+  return (
+    <>
+      <div className="flex justify-center items-center gap-2 w-full">
+        <SearchBox
+          placeholder={placeholder}
+          className="w-full lg:w-2/4"
+        />
+
+        <div className="flex items-center gap-2">
+          <Button icon="mdi:sort" type="outline-info">
+            Sort
+          </Button>
+
+          <Button icon="mdi:filter-outline" type="outline-info" onClick={openModal}>
+            Filter
+          </Button>
+        </div>
+      </div>
+
+      <FilterModal categories={categories} isOpen={open} onClose={closeModal} />
+    </>
+  );
+}
