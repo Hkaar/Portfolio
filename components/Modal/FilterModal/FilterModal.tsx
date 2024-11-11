729d1a490e2efe557553aabc9d990b4ecb5de870
Hkaar <HkaarDev@gmail.com>
2024-11-11 16:34:44 +0800

    Updated filter modal

--- components/Modal/FilterModal/FilterModal.tsx
+++ components/Modal/FilterModal/FilterModal.tsx
@@ -3,7 +3,7 @@ import Modal, { ModalProps } from "../Modal";
 import { Icon } from "@iconify/react";
 import { twMerge } from "tailwind-merge";
 import { Category } from "@/types/contentUtils";
-import CategoryBadge from "@/components/Badge/CategoryBadge";
+import CategoryButton from "@/components/Button/CategoryButton";
 
 interface FilterModalProps extends Omit<ModalProps, "children"> {
   categories: Category[];
@@ -16,7 +16,7 @@ export default function FilterModal(
     <Modal
       isOpen={props.isOpen}
       onClose={props.onClose}
-      className={twMerge("w-1/2 h-3/4", props.className)}
+      className={twMerge("w-1/2 h-3/4 flex flex-col", props.className)}
     >
       <div className="flex justify-between items-center gap-8 px-4 pt-3 mb-3">
         <h6 className="text-xl font-semibold">
@@ -28,12 +28,12 @@ export default function FilterModal(
         </Button>
       </div>
 
-      <div className="flex flex-col gap-6 px-4 mb-3">
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-3 px-3 py-2 rounded-md border">
+      <div className="flex flex-col gap-6 px-4 mb-3 flex-1">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 px-3 py-2 rounded-md border flex-1">
           {categories.map((category, i) => (
-            <CategoryBadge key={`${category.title}${i}`} icon={category.icon} slug={category.title} className="shadow">
+            <CategoryButton key={`${category.title}${i}`} icon={category.icon} slug={category.title} className="shadow w-full">
               {category.title}
-            </CategoryBadge>
+            </CategoryButton>
           ))}
         </div>
       </div>
