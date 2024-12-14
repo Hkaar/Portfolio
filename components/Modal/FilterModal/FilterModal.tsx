678d68b347ccd21878d2e3c0b27057d55ded9cc9
Hkaar <HkaarDev@gmail.com>
2024-12-14 17:44:14 +0800

    Updated modals

--- components/Modal/FilterModal/FilterModal.tsx
+++ components/Modal/FilterModal/FilterModal.tsx
@@ -1,8 +1,10 @@
-import Button from "@/components/Button";
-import Modal, { ModalProps } from "../Modal";
-import { Icon } from "@iconify/react";
 import { twMerge } from "tailwind-merge";
 import { Category } from "@/types/contentUtils";
+
+import { CircleX } from "lucide-react";
+
+import Button from "@/components/Button";
+import Modal, { ModalProps } from "@/components/Modal/Modal";
 import CategoryButton from "@/components/Button/CategoryButton";
 
 interface FilterModalProps extends Omit<ModalProps, "children"> {
@@ -23,13 +25,13 @@ export default function FilterModal(
     >
       <div className="flex flex-col gap-2">
         <div className="flex items-center justify-between gap-8">
-          <span className="text-3xl font-semibold">
+          <span className="text-3xl font-bold">
             Filter Search
           </span>
 
           <Button onClick={() => props.onClose()} className="border-0 p-0">
-          <Icon icon="mdi:close" fontWeight={300} />
-        </Button>
+            <CircleX size={24} strokeWidth={1.5} className="stroke-danger dark:stroke-danger-dark" />
+          </Button>
         </div>
 
         <span className="text-gray-500 dark:text-neutral-600 text-sm">
@@ -37,7 +39,16 @@ export default function FilterModal(
         </span>
       </div>
 
-      <div className="flex flex-col gap-6 mb-3 flex-1 overflow-y-auto overflow-x-hidden">
+      <div
+        className={`
+          flex flex-col gap-6 mb-3 flex-1 overflow-y-auto overflow-x-hidden [&::-webkit-scrollbar]:w-2
+          [&::-webkit-scrollbar-track]:rounded-full
+        [&::-webkit-scrollbar-track]:bg-gray-100
+          [&::-webkit-scrollbar-thumb]:rounded-full
+        [&::-webkit-scrollbar-thumb]:bg-gray-300
+        dark:[&::-webkit-scrollbar-track]:bg-neutral-700
+        dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 px-2`}
+      >
         <div className="grid grid-cols-2 md:grid-cols-3 gap-2 flex-1">
           {categories.map((category, i) => (
             <CategoryButton
